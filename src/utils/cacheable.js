64218ecc3b770e03b7d9da2dd6f4c71297ee72c9
Alphmega <greedisgood5000@163.com>
2017-11-22 16:18:45 +0800

    update delay方法增加结果 update cacheable修改默认key以及修改第二参数

--- src/utils/cacheable.js
+++ src/utils/cacheable.js
@@ -2,15 +2,17 @@ import $isPlainObject from './is-plain-object';
 import $extend from './extend';
 import $is from './is';
 import $delay from './delay';
-const defaultCacheMap = new Map();
+const defaultCacheMapSymbol = Symbol();
+const cacheIdSymbol = Symbol();
+let id = 0;
 export default function cacheable(fn, options) {
   if (!$isPlainObject(options)) {
     options = {
-      key: options,
+      expires: options,
     };
   }
   options = $extend({}, options, cacheable.defaults);
-  if (!$is(options.key, 'function')) {
+  if (options.key && !$is(options.key, 'function')) {
     const tmpKey = options.key;
     options.key = () => tmpKey;
   }
@@ -18,10 +20,14 @@ export default function cacheable(fn, options) {
     const tmpExpires = options.expires;
     options.expires = async (result) => { await result; await $delay(tmpExpires); };
   }
-  return function (...args) {
+  return function fnResult(...args) {
     const context = options.context || this;
-    const cacheKey = options.key.call(context, ...args);
-    let cacheItem = options.get.call(context, cacheKey, options.map);
+    fn[cacheIdSymbol] = fn[cacheIdSymbol] || id++;
+    context[cacheIdSymbol] = context[cacheIdSymbol] || id++;
+    const cacheKey = options.key ? options.key.call(context, ...args) : `${fn[cacheIdSymbol]}-${context[cacheIdSymbol]}`;
+    const map = $is(options.map, 'function') ? options.map.call(context) : options.map;
+
+    let cacheItem = options.get.call(context, cacheKey, map);
     if (cacheItem) {
       const obstruction = typeof options.obstruction === 'function' ? options.obstruction.call(context, ...args) : options.obstruction;
       if (obstruction) {
@@ -29,15 +35,16 @@ export default function cacheable(fn, options) {
       }
     } else {
       cacheItem = fn.call(this, ...args);
-      options.set.call(context, cacheKey, cacheItem, options.map);
+      options.set.call(context, cacheKey, cacheItem, map);
       options.expires.call(context, cacheItem).then(() => {
-        options.clear.call(context, cacheKey, options.map);
+        options.clear.call(context, cacheKey, map);
       });
     }
     return cacheItem;
-  };
+  }
 }
 cacheable.defaults = {
+  // 调用config各种方法的上下文，如果为undefined，则为结果function的上下文
   context: undefined,
   // 设置cache的过期时间或者方法
   // number，设置n毫秒后清除缓存
@@ -49,12 +56,17 @@ cacheable.defaults = {
   // object 阻塞，则抛出该对象
   // 或者 function 获取返回值后按照上述规则
   obstruction: false,
-  // 设置缓存的key
+  // 设置缓存的key，如果为undefined，则自动生成
+  key: undefined,
+  /*
   key() {
     return this;
   },
-  // map参数和get等参数二选一
-  map: defaultCacheMap,
+  */
+  // 缓存表或者获取缓存表的方法;
+  map() {
+    return this[defaultCacheMapSymbol] = this[defaultCacheMapSymbol] || new Map();
+  },
   // 获取设置清除缓存的方法
   get(r, map) {
     return map.get(r);
--- src/utils/delay.js
+++ src/utils/delay.js
@@ -1,12 +1,12 @@
-export default function (t) {
+export default function (t, result) {
   if (t === Infinity) {
     return new Promise(() => {});
   }
   if (t instanceof Date) {
     t = (+ t) - Date.now();
   }
-  if (t <= 0) return Promise.resolve();
+  if (t <= 0) return Promise.resolve(result);
   return new Promise((r) => {
-    setTimeout(r, t);
+    setTimeout(() => { r(result) }, t);
   });
 }
