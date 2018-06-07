103f113d22a1af0236c695abceacb04967cf6f16
Alphmega <greedisgood5000@163.com>
2018-06-07 17:47:25 +0800

    fix: 避免chrome 47以下不支持new Regexp(regexp, flag)传递flag的错误

--- src/utils/is.js
+++ src/utils/is.js
@@ -3,7 +3,7 @@ export default function is(obj, type) {
   if (typeof type === 'function') return obj instanceof type;
   const result = emptyObj.toString.call(obj).slice(8, -1);
   if (type) {
-    if (!type instanceof RegExp) {
+    if (!(type instanceof RegExp)) {
       type = new RegExp(type, 'gi')
     }
     return type.test(result)
